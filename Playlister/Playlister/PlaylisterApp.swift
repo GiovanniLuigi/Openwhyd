809a7235654955532b94564e9a093caa65dbe5dc
Giovanni <giovannibruno128@gmail.com>
2021-04-16 20:43:11 -0300

    Created project

---
+++ Playlister/Playlister/PlaylisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylisterApp.swift
+//  Playlister
+//
+//  Created by Giovanni Luidi Bruno on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaylisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
